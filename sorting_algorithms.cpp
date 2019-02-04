61fb76ad59a636226b50a155fad689e2a1606699
Maxi Pineker <maxi.pineker@gmail.com>
2019-02-04 21:59:44 +0100

    implemented the sorts

--- sorting_algorithms.cpp
+++ sorting_algorithms.cpp
@@ -10,17 +10,45 @@
  * <your description here>
  *-----------------------------------------------------------------------------
 */
-#include "sorting_algorithms.h"
+#include <cstdlib>
+#include <cstdbool>
+#include "general.h"
+#include <time.h>
 
 void bubble_sort(int small_array[], int length)
 {
+  bool isSwapped = false;
+	do{
+		isSwapped = false;
 
+		for(unsigned long i = 0; i < length-1; i++){
+			if(small_array[i] > small_array[i+1]){
+				int tmp = small_array[i];
+				small_array[i] = small_array[i+1];
+				small_array[i+1] = tmp;
+				isSwapped = true;
+			}
+		}
+	}while(isSwapped);
 }
 void insertion_sort(int small_array[], int length)
 {
+  int j = 0;
+	for(int i = 1; i < length; i++)
+	{
+		j = i;
+		while(j>0 && small_array[j - 1] > small_array[j]){
+			int tmp = small_array[i];
+			small_array[i] = small_array[i-1];
+			small_array[i+1] = tmp;
+			j--;
+		}
+	}
 
 }
 void init_random(int small_array[], int length)
 {
-
+  srandom(time(NULL));
+	for (unsigned long i = 0; i < length; i++)
+		small_array[i] = random();
 }
